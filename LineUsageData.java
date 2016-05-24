4df0710ad07219fc90277828c2e79cf48022ce51
John Allen <johnallen@Johns-MBP.fios-router.home>
2016-05-23 23:18:43 -0400

    added: LineUsageData.java version 3

--- LineUsageData.java
+++ LineUsageData.java
@@ -1,18 +1,41 @@
 public class LineUsageData {
 
-	private SinglyLinkedList<Usage> list = new SinglyLinkedList<Usage>();
+	private SinglyLinkedList<Usage> list;
+
+	public LineUsageData() {
+		list = new SinglyLinkedList<Usage>();
+	}
 
 	public void addObservation(String username) {
-		list.add(new Usage(username, 0));
+		if (this.list.size() == 0)
+			this.list.add(new Usage(username, 1));
+		else {
+			boolean present = false;
+			int index;
+			for (index = 0; index < this.list.size(); index++) {
+				if (username.equals(this.list.get(index).getUser())) 
+					present = true;
+				if (present)
+					break;
+			}
+			if (present)
+				this.list.get(index).addCount();
+			else
+				this.list.add(new Usage(username, 1));
+		}
 	}
 
 	public Usage findMaxUsage() {
-		Usage result;
-		for (int i = 0; i < list.size(); i++) {
-			if (list.get(i).count > result.count)
+		Usage result = new Usage(null, 0);
+		for (int i = 0; i < this.list.size(); i++) {
+			if (list.get(i).getCount() > result.getCount())
 				result = list.get(i);		
-		}		
+		}
 		return result;
 	}
+
+	public int size() {
+                return this.list.size();
+        }
 
 }
